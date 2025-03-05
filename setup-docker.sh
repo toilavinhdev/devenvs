3cf01d5adfbeff20b14001f8c84e477226737440
toilavinh <hoangdvinh68@gmail.com>
2025-03-05 23:12:18 +0700

    just update

---
+++ setup-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Install prerequisite packages
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+# Add GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add to package manager
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install docker
+sudo apt update
+sudo apt install docker-ce
+
+# Add current user to docker group (Execute docker without sudo)
+sudo usermod -aG docker ${USER}
+
+# Install docker compose plugins
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
---
+++ setup-jenkins.sh
@@ -0,0 +1,16 @@
+# Install Java JDK
+sudo apt install fontconfig openjdk-17-jre
+
+# Add GPG key
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+# Add to package manager
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Install jenkins
+sudo apt-get update
+sudo apt-get install jenkins
+
+# Add jenkins to docker group
+usermod -aG docker jenkins
+sudo systemctl restart jenkins.service
